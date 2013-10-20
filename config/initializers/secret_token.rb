5f8bb572d69d8e0397f09a6f777293343cf61bfb
Daniel Carvalho <dapcarvalho@gmail.com>
2013-10-20 02:45:17 +0000

    Initializes repo with ready rails app for deployment on heroku with newrelic's gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Relic::Application.config.secret_key_base = '2087888fb21e28278b9a2507a648522bdaa60c114eed63d2846f5bddd213109c13d0168a975a8fe01487b43d2e20641cef0d4f46e267c9364302557607f13d28'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Relic::Application.config.session_store :cookie_store, key: '_relic_session'
